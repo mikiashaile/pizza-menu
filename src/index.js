ac138c79c2f308a56b7edd23e7de87660b32d3df
mikias.haile <mikias.haile@accenture.com>
2023-09-12 12:02:22 -0400

    pizza menu component initial

--- src/index.js
+++ src/index.js
@@ -2,7 +2,41 @@ import React from "react";
 import ReactDom from "react-dom/client";
 
 function App() {
-  return <h1>Hello Pizza App</h1>;
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Pizzas() {
+  return (
+    <div>
+      <img src="pizzas/focaccia.jpg" alt="ficcacia" />
+      <h2>Focaccia</h2>
+      <p>Bread with italian olive oil and rosemary</p>
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Mission Impizzable Pizzeria</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <Pizzas />
+      <Pizzas />
+      <Pizzas />
+    </div>
+  );
+}
+
+function Footer() {
+  return <footer>We are open {new Date().toLocaleDateString()}</footer>;
 }
 
 const root = ReactDom.createRoot(document.getElementById("root"));
