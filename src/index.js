dfa25015d8410eb42c0a6bd5fb5fea5217c9b269
mikias.haile <mikias.haile@accenture.com>
2023-09-13 12:03:23 -0400

    add a list component from data

--- src/index.js
+++ src/index.js
@@ -2,6 +2,51 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import "./index.css";
 
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
 function App() {
   return (
     <div className="container">
@@ -12,16 +57,16 @@ function App() {
   );
 }
 
-function Pizzas(props) {
+function Pizza(props) {
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
@@ -36,24 +81,12 @@ function Header() {
 function Menu() {
   return (
     <main className="menu">
-      <Pizzas
-        name="Focaccia"
-        price={6}
-        photoName="pizzas/focaccia.jpg"
-        ingredients="Bread with italian olive oil and rosemary"
-      />
-      <Pizzas
-        name="Pizza Margherita"
-        price={10}
-        photoName="pizzas/margherita.jpg"
-        ingredients="Tomato and mozarella"
-      />
-      <Pizzas
-        name="Pizza Spinaci"
-        price={12}
-        photoName="pizzas/spinaci.jpg"
-        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-      />
+      <h2>Our Menu</h2>
+      <ul className="pizzas">
+        {pizzaData.map((pizza, index) => (
+          <Pizza pizzaObj={pizza} key={index} />
+        ))}
+      </ul>
     </main>
   );
 }
